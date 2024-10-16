8a4105a04e0a1805f6af5dd518b8c6a0bcbc4d5f
ualdrm <aldar.mandzhiev@inbox.ru>
2024-10-17 01:04:10 +0300

    Models+Floor+Walls+Light

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ float zalfa = 0;
 double lastX = 250.0;
 double lastY = 250.0;
 bool firstMouse = true;
+float cameraSpeed = 0.05f;
+vec3 cameraPos = vec3(0.0f, 0.0f, 5.0f);
+
+float timeOfDay = 0.0f; 
+float dayDuration = 10.0f;
+float nightDuration = 10.0f; 
 
 const char* vertex_shader_source = R"(
     #version 330 core
@@ -30,13 +36,24 @@ const char* vertex_shader_source = R"(
     }
 )";
 
-const char* fragment_shader_source = R"(
+const char* fragment_shader_source_solid = R"(
+    #version 330 core
+    out vec4 fragColor;
+    void main() {
+        fragColor = vec4(1.0, 1.0, 1.0, 1.0); // Белый цвет
+    }
+)";
+
+const char* fragment_shader_source_texture = R"(
     #version 330 core
     out vec4 fragColor;
     in vec2 TexCoord;
     uniform sampler2D texture1;
+    uniform vec3 lightColor; // Цвет света
+    uniform vec3 lightPos; // Позиция света
     void main() {
-        fragColor = texture(texture1, TexCoord);
+        vec4 texColor = texture(texture1, TexCoord);
+        fragColor = texColor * vec4(lightColor, 1.0); // Умножаем цвет текстуры на цвет света
     }
 )";
 
@@ -78,22 +95,17 @@ public:
         glDeleteShader(fragmentShader);
         checkProgram(program);
     }
-    
     ~Shader() {
         glDeleteProgram(program);
     }
-
     void use() {
         glUseProgram(program);
     }
-
     GLuint getProgram() const {
         return program;
     }
-
 private:
     GLuint program;
-
     GLuint compileShader(GLenum type, const char* src) {
         GLuint shader = glCreateShader(type);
         glShaderSource(shader, 1, &src, nullptr);
@@ -101,7 +113,6 @@ private:
         checkShader(shader);
         return shader;
     }
-
     void checkShader(GLuint shader) {
         GLint success;
         glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
@@ -113,7 +124,6 @@ private:
             cerr << "Shader compile error: " << log.data() << endl;
         }
     }
-
     void checkProgram(GLuint program) {
         GLint success;
         glGetProgramiv(program, GL_LINK_STATUS, &success);
@@ -129,47 +139,160 @@ private:
 
 class ShapeRenderer {
 public:
-    ShapeRenderer(const vector<float>& vertices, const vector<float>& texCoords) {
+    ShapeRenderer(const vector<float>& vertices) {
         glGenVertexArrays(1, &vao);
         glGenBuffers(1, &vbo);
-        glGenBuffers(1, &tbo);
         glBindVertexArray(vao);
-        
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
-        
-        glBindBuffer(GL_ARRAY_BUFFER, tbo);
-        glBufferData(GL_ARRAY_BUFFER, texCoords.size() * sizeof(float), texCoords.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
-        
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
     }
-
     ~ShapeRenderer() {
         glDeleteVertexArrays(1, &vao);
         glDeleteBuffers(1, &vbo);
-        glDeleteBuffers(1, &tbo);
     }
-
     void render(Shader& shader, GLuint texture, int count) {
         shader.use();
         glBindTexture(GL_TEXTURE_2D, texture); 
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, count);
+        glDrawArrays(GL_TRIANGLES, 0, count);
     }
-
 private:
-    GLuint vao, vbo, tbo;
+    GLuint vao, vbo;
 };
 
-GLuint loadTexture() {
+vector<float> generateSphereVertices(float radius, int sectorCount, int stackCount) {
+    vector<float> vertices;
+    for (int i = 0; i <= stackCount; ++i) {
+        float stackAngle = M_PI / 2 - i * M_PI / stackCount; 
+        float xy = radius * cosf(stackAngle);
+        float z = radius * sinf(stackAngle); 
+        for (int j = 0; j <= sectorCount; ++j) {
+            float sectorAngle = j * 2 * M_PI / sectorCount; 
+            float x = xy * cosf(sectorAngle); 
+            float y = xy * sinf(sectorAngle); 
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
+            
+            float s = (float)j / sectorCount;
+            float t = (float)i / stackCount; 
+            vertices.push_back(s);
+            vertices.push_back(t);
+        }
+    }
+    return vertices;
+}
+
+vector<float> generateCubeVertices() {
+    return {
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+         0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    };
+}
+
+vector<float> generatePlaneVertices() {
+    return {
+        -50.0f, 0.0f, -50.0f, 0.0f, 0.0f,
+         50.0f, 0.0f, -50.0f, 1.0f, 0.0f,
+         50.0f, 0.0f,  50.0f, 1.0f, 1.0f,
+         50.0f, 0.0f,  50.0f, 1.0f, 1.0f,
+        -50.0f, 0.0f,  50.0f, 0.0f, 1.0f,
+        -50.0f, 0.0f, -50.0f, 0.0f, 0.0f,
+    };
+}
+
+vector<float> generateWallVertices() {
+    return {
+        -50.0f, -1.0f, -50.0f, 0.0f, 1.0f,
+         50.0f, -1.0f, -50.0f, 1.0f, 1.0f,
+         50.0f,  50.0f, -50.0f, 1.0f, 0.0f,
+         50.0f,  50.0f, -50.0f, 1.0f, 0.0f,
+        -50.0f,  50.0f, -50.0f, 0.0f, 0.0f,
+        -50.0f, -1.0f, -50.0f, 0.0f, 1.0f,
+        
+        -50.0f, -1.0f, 50.0f, 1.0f, 1.0f, 
+         50.0f, -1.0f, 50.0f, 0.0f, 1.0f, 
+         50.0f,  50.0f, 50.0f, 0.0f, 0.0f,
+         50.0f,  50.0f, 50.0f, 0.0f, 0.0f,
+        -50.0f,  50.0f, 50.0f, 1.0f, 0.0f,
+        -50.0f, -1.0f, 50.0f, 1.0f, 1.0f, 
+        
+        -50.0f, -1.0f, -50.0f, 0.0f, 1.0f,
+        -50.0f, -1.0f, 50.0f, 1.0f, 1.0f,
+        -50.0f,  50.0f, 50.0f, 1.0f, 0.0f,
+        -50.0f,  50.0f, 50.0f, 1.0f, 0.0f,
+        -50.0f,  50.0f, -50.0f, 0.0f, 0.0f,
+        -50.0f, -1.0f, -50.0f, 0.0f, 1.0f,
+        
+        50.0f, -1.0f, -50.0f, 1.0f, 1.0f, 
+        50.0f, -1.0f, 50.0f, 0.0f, 1.0f,
+        50.0f,  50.0f, 50.0f,  0.0f, 0.0f,
+        50.0f,  50.0f, 50.0f,  0.0f, 0.0f,
+        50.0f,  50.0f, -50.0f, 1.0f, 0.0f,
+        50.0f, -1.0f, -50.0f, 1.0f, 1.0f, 
+    };
+}
+
+vector<float> generateCeilingVertices() {
+    return {
+        -50.0f, 50.0f, -50.0f, 0.0f, 0.0f,
+         50.0f, 50.0f, -50.0f, 1.0f, 0.0f,
+         50.0f, 50.0f,  50.0f, 1.0f, 1.0f,
+         50.0f, 50.0f,  50.0f, 1.0f, 1.0f,
+        -50.0f, 50.0f,  50.0f, 0.0f, 1.0f,
+        -50.0f, 50.0f, -50.0f, 0.0f, 0.0f,
+    };
+}
+
+GLuint loadTexture(const char* path) {
     GLuint textureID;
     glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID); 
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -177,18 +300,75 @@ GLuint loadTexture() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("/home/ualdrm/Studies/KR/eagle.jpeg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
     if (data) {
-        GLenum format = (nrChannels == 1) ? GL_RED : (nrChannels == 3) ? GL_RGB : GL_RGBA;
+        GLenum format;
+        if (nrChannels == 1) {
+            format = GL_RED;
+        } else if (nrChannels == 3) {
+            format = GL_RGB;
+        } else if (nrChannels == 4) {
+            format = GL_RGBA;
+        }
+
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     } else {
-        cerr << "Failed to load texture: " << endl;
-        cerr << "stbi_load error: " << stbi_failure_reason() << endl;
+        cerr << "Не удалось загрузить текстуру: " << path << endl;
+        cerr << "stbi_load ошибка: " << stbi_failure_reason() << endl;
         return 0;
     }
-    stbi_image_free(data);
+
+    stbi_image_free(data); 
     return textureID;
+}
+
+vector<float> generatePyramidVertices() {
+    return {
+        
+        -0.5f, 0.0f, -0.5f, 0.0f, 0.0f,
+         0.5f, 0.0f, -0.5f, 1.0f, 0.0f,
+         0.5f, 0.0f,  0.5f, 1.0f, 1.0f,
+         0.5f, 0.0f,  0.5f, 1.0f, 1.0f,
+        -0.5f, 0.0f,  0.5f, 0.0f, 1.0f,
+        -0.5f, 0.0f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, 0.0f, -0.5f, 0.0f, 0.0f,
+         0.5f, 0.0f, -0.5f, 1.0f, 0.0f,
+         0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+
+         0.5f, 0.0f, -0.5f, 1.0f, 0.0f,
+         0.5f, 0.0f,  0.5f, 1.0f, 1.0f,
+         0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+
+         0.5f, 0.0f,  0.5f, 1.0f, 1.0f,
+        -0.5f, 0.0f,  0.5f, 0.0f, 1.0f,
+         0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+
+        -0.5f, 0.0f,  0.5f, 0.0f, 1.0f,
+        -0.5f, 0.0f, -0.5f, 0.0f, 0.0f,
+         0.0f, 1.0f, 0.0f, 0.5f, 1.0f  
+    };
+}
+
+void processInput(GLFWwindow *window) {
+    vec3 front;
+    front.x = cos(radians(zalfa)) * cos(radians(alfa));
+    front.y = sin(radians(alfa));
+    front.z = sin(radians(zalfa)) * cos(radians(alfa));
+    front = normalize(front);
+
+    vec3 right = normalize(cross(front, vec3(0.0f, 1.0f, 0.0f))); 
+    vec3 up = normalize(cross(right, front)); 
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += front * cameraSpeed; 
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= front * cameraSpeed; 
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= right * cameraSpeed; 
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += right * cameraSpeed; 
 }
 
 int main() {
@@ -199,56 +379,110 @@ int main() {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        
-        GLFWwindow* window = glfwCreateWindow(500, 500, "GRAPH-3", nullptr, nullptr);
+        GLFWwindow* window = glfwCreateWindow(800, 600, "GRAPH-3", nullptr, nullptr);
         if (!window) {
             glfwTerminate();
             throw runtime_error("Window create error");
         }
         glfwMakeContextCurrent(window);
+        glfwSetCursorPosCallback(window, mouse_callback);
+        glfwSetKeyCallback(window, key_callback);
+        
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
         if (glewInit() != GLEW_OK) {
             throw runtime_error("Glew error");
         }
-        
-        glfwSetCursorPosCallback(window, mouse_callback);
 
-        vector<float> floorVertices = {
-            1.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            -1.0f, -1.0f, 0.0f,
-            -1.0f, 1.0f, 0.0f
-        };
-        vector<float> floorTexCoords = {
-            1.0f, 1.0f,
-            1.0f, 0.0f,
-            0.0f, 0.0f,
-            0.0f, 1.0f
-        };
+        float radius = 1.0f;
+        int sectorCount = 36;
+        int stackCount = 18;
+        vector<float> sphereVertices = generateSphereVertices(radius, sectorCount, stackCount);
+        ShapeRenderer sphereRenderer(sphereVertices);
 
-        ShapeRenderer floorRenderer(floorVertices, floorTexCoords);
-        Shader shader(vertex_shader_source, fragment_shader_source);
-        GLuint transformLoc = glGetUniformLocation(shader.getProgram(), "transform");
-        GLuint texture = loadTexture();
+        Shader shaderSolid(vertex_shader_source, fragment_shader_source_solid);
+        Shader shaderTexture(vertex_shader_source, fragment_shader_source_texture);
+        GLuint transformLoc = glGetUniformLocation(shaderSolid.getProgram(), "transform");
+        GLuint texture = loadTexture("/home/ualdrm/Studies/KR/metal.jpeg");
+        GLuint floorTexture = loadTexture("/home/ualdrm/Studies/KR/water.jpg"); 
+        GLuint wallTexture = loadTexture("/home/ualdrm/Studies/KR/cosmos.jpg");
+        GLuint topTexture = loadTexture("/home/ualdrm/Studies/KR/cosm.jpg"); 
+        vector<float> planeVertices = generatePlaneVertices();
+        ShapeRenderer planeRenderer(planeVertices);
+        ShapeRenderer pyramidRenderer(generatePyramidVertices());
 
-        if (texture == 0) {
-            throw runtime_error("Failed to load texture");
-        }
+        vector<float> cubeVertices = generateCubeVertices();
+        ShapeRenderer cubeRenderer(cubeVertices);
+
+        vector<float> wallVertices = generateWallVertices();
+        ShapeRenderer wallRenderer(wallVertices);
+
+        vector<float> ceilingVertices = generateCeilingVertices();
+        ShapeRenderer ceilingRenderer(ceilingVertices);
 
         while (!glfwWindowShouldClose(window)) {
+            timeOfDay += (1.0f / 60.0f);
+            if (timeOfDay > (dayDuration + nightDuration)) {
+                timeOfDay = 0.0f;
+            }
+
+            vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+            vec3 lightPos = vec3(5.0f * cos(timeOfDay * 2.0f * M_PI / (dayDuration + nightDuration)), 
+                      5.0f * sin(timeOfDay * 2.0f * M_PI / (dayDuration + nightDuration)), 
+                      0.0f);
+            if (timeOfDay < dayDuration) {
+                float intensity = timeOfDay / dayDuration; 
+                lightColor *= intensity; 
+            } else {
+                float intensity = 1.0f - ((timeOfDay - dayDuration) / nightDuration);
+                lightColor *= intensity; 
+            }
+
             glEnable(GL_DEPTH_TEST);
-            glfwSetKeyCallback(window, key_callback);
+            processInput(window);
             glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            mat4 transformMatrix = mat4(1.0f);
-            mat4 projection = perspective(radians(45.0f), 500.0f / 500.0f, 0.1f, 100.0f);
-            mat4 model = translate(transformMatrix, vec3(0, 0, 0));
-            mat4 view = lookAt(vec3(0, 0, 3), vec3(0, 0, 0), vec3(0, 1, 0));
-            view = rotate(view, radians(alfa), vec3(1, 0, 0));
-            view = rotate(view, radians(zalfa), vec3(0, 1, 0));
+            mat4 model = mat4(1.0f);
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(model));
+            glUniform3fv(glGetUniformLocation(shaderTexture.getProgram(), "lightColor"), 1, value_ptr(lightColor));
+            glUniform3fv(glGetUniformLocation(shaderTexture.getProgram(), "lightPos"), 1, value_ptr(lightPos));
+            planeRenderer.render(shaderTexture, floorTexture, planeVertices.size() / 5);
 
+            mat4 transformMatrix = mat4(1.0f);
+            mat4 projection = perspective(radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+             model = translate(transformMatrix, vec3(0, 0, 0));
+            mat4 view;
+            vec3 front;
+            front.x = cos(radians(zalfa)) * cos(radians(alfa));
+            front.y = sin(radians(alfa));
+            front.z = sin(radians(zalfa)) * cos(radians(alfa));
+            front = normalize(front);
+            view = lookAt(cameraPos, cameraPos + front, vec3(0, 1, 0));
             glUniformMatrix4fv(transformLoc, 1, GL_FALSE, value_ptr(projection * view * model));
-            //floorRenderer.render(shader, texture, floorVertices.size() / 3);
+            mat4 floorModel = translate(mat4(1.0f), vec3(0.0f, -1.0f, 0.0f));
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * floorModel));
+            planeRenderer.render(shaderTexture, floorTexture, planeVertices.size() / 5);
+            
+            mat4 sphereModel = translate(mat4(1.0f), vec3(0, -0.3, 0)); 
+            glUniformMatrix4fv(glGetUniformLocation(shaderSolid.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * sphereModel));
+            sphereRenderer.render(shaderSolid, 0, sphereVertices.size() / 5);
+
+            mat4 cubeModel = translate(mat4(1.0f), vec3(2, 0, 0)); 
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * cubeModel));
+            cubeRenderer.render(shaderTexture, texture, cubeVertices.size() / 5);
+
+            mat4 pyramidModel = translate(mat4(1.0f), vec3(-2.0f, -0.8f, 0.0f));
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * pyramidModel));
+            pyramidRenderer.render(shaderTexture, texture, 18);
+
+            mat4 wallModel = mat4(1.0f);
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * wallModel));
+            wallRenderer.render(shaderTexture, wallTexture, wallVertices.size() / 5);
+            
+            mat4 ceilingModel = mat4(1.0f);
+            glUniformMatrix4fv(glGetUniformLocation(shaderTexture.getProgram(), "transform"), 1, GL_FALSE, value_ptr(projection * view * ceilingModel));
+            ceilingRenderer.render(shaderTexture, topTexture, ceilingVertices.size() / 5);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
